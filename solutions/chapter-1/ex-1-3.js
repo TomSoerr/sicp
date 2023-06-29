783c3441a185baac7fe768a4bd0dffb09932d06e
TomSoerr <tomix2004@gmail.com>
2023-06-29 13:14:15 +0200

    Add solutions up to and including exercise 1.5

--- solutions/chapter-1/ex-1-3.js
+++ solutions/chapter-1/ex-1-3.js
@@ -1,4 +1,16 @@
-function sumOfSquaresOfTwoBiggestNumbers(a, b, c) {
-  const biggestNumber = a < b || 
-  return 
+/* eslint-disable no-nested-ternary */
+function sumOfSquares(a, b) {
+  return a * a + b * b;
 }
+
+function sumOfSquaresOfTwoBiggestNumbers(a, b, c) {
+  return a < b && a < c
+    ? sumOfSquares(b, c)
+    : b < a && b < c
+    ? sumOfSquares(a, c)
+    : sumOfSquares(a, b);
+}
+
+sumOfSquaresOfTwoBiggestNumbers(1, 2, 3); // 13
+sumOfSquaresOfTwoBiggestNumbers(2, 1, 3); // 13
+sumOfSquaresOfTwoBiggestNumbers(2, 3, 1); // 13
