40f96b103001341badf2a31f2dc08234e1d44549
TomSoerr <tomix2004@gmail.com>
2024-12-25 10:36:11 +0100

    Add solution for exercise 1.13

--- 1-chapter/Exercise1.12.js
+++ 1-chapter/Exercise1.12.js
@@ -2,4 +2,6 @@ function n_over_k(n, k) {
   return k === n || k === 0 ? 1 : n_over_k(n - 1, k - 1) + n_over_k(n - 1, k);
 }
 
-// console.log(n_over_k(4, 2)); // => 6
+console.log(n_over_k(4, 2)); // => 6
+
+
