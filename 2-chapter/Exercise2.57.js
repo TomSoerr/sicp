4f09aaceb4d32d30a9f897d85379083de21f5bc6
TomSoerr <tomix2004@gmail.com>
2025-02-04 11:54:13 +0100

    Add solution for 2.56 and 2.57

--- 2-chapter/Exercise2.57.js
+++ 2-chapter/Exercise2.57.js
@@ -7,6 +7,7 @@ import {
   head,
   tail,
   list,
+  is_null,
 } from 'sicp';
 
 const is_variable = (x) => is_string(x);
@@ -18,52 +19,7 @@ const is_sum = (x) => is_pair(x) && head(x) === '+';
 
 const number_equal = (exp, num) => is_number(exp) && exp === num;
 
-const make_sum = (a1, a2) =>
-  number_equal(a1, 0) ? a2
-  : number_equal(a2, 0) ? a1
-  : is_number(a1) && is_number(a2) ? a1 + a2
-  : list('+', a1, a2);
-
-const make_product = (m1, m2) =>
-  number_equal(m1, 0) || number_equal(m2, 0) ? 0
-  : number_equal(m1, 1) ? m2
-  : number_equal(m2, 1) ? m1
-  : is_number(m1) && is_number(m2) ? m1 * m2
-  : list('*', m1, m2);
-
-const addend = (s) => head(tail(s));
-
-const augend = (s) => head(tail(tail(s)));
-
 const is_product = (x) => is_pair(x) && head(x) === '*';
-
-const multiplier = (s) => head(tail(s));
-
-const multiplicand = (s) => head(tail(tail(s)));
-
-function deriv(exp, variable) {
-  return (
-    is_number(exp) ? 0
-    : is_variable(exp) ?
-      is_same_variable(exp, variable) ? 1
-      : 0
-    : is_sum(exp) ?
-      make_sum(deriv(addend(exp), variable), deriv(augend(exp), variable))
-    : is_product(exp) ?
-      make_sum(
-        make_product(multiplier(exp), deriv(multiplicand(exp), variable)),
-        make_product(deriv(multiplier(exp), variable), multiplicand(exp)),
-      )
-      // above given by exercise
-    : is_exp(exp) ?
-      make_product(
-        exponent(exp),
-        make_exp(base(exp), exponent(exp) - 1),
-        deriv(base(exp), variable),
-      )
-    : error(exp, 'unknown expression type -- deriv')
-  );
-}
 
 function is_exp(x) {
   return is_pair(x) && head(x) === '**';
@@ -85,4 +41,55 @@ function make_exp(e1, e2) {
   );
 }
 
-display(deriv(list('**', 'x', 4), 'x')); // ['*', [4, [['**', ['x', [3, null]]], null]]];
+const make_sum = (a1, a2) =>
+  number_equal(a1, 0) ? a2
+  : number_equal(a2, 0) ? a1
+  : is_number(a1) && is_number(a2) ? a1 + a2
+  : list('+', a1, a2);
+
+const make_product = (m1, m2) =>
+  number_equal(m1, 0) || number_equal(m2, 0) ? 0
+  : number_equal(m1, 1) ? m2
+  : number_equal(m2, 1) ? m1
+  : is_number(m1) && is_number(m2) ? m1 * m2
+  : list('*', m1, m2);
+
+function deriv(exp, variable) {
+  return (
+    is_number(exp) ? 0
+    : is_variable(exp) ?
+      is_same_variable(exp, variable) ? 1
+      : 0
+    : is_sum(exp) ?
+      make_sum(deriv(addend(exp), variable), deriv(augend(exp), variable))
+    : is_product(exp) ?
+      make_sum(
+        make_product(multiplier(exp), deriv(multiplicand(exp), variable)),
+        make_product(deriv(multiplier(exp), variable), multiplicand(exp)),
+      )
+    : is_exp(exp) ?
+      make_product(
+        exponent(exp),
+        make_exp(base(exp), exponent(exp) - 1),
+        deriv(base(exp), variable),
+      )
+    : error(exp, 'unknown expression type -- deriv')
+  );
+}
+
+const addend = (s) => head(tail(s));
+const multiplier = (s) => head(tail(s));
+
+// above given by exercise
+
+const augend = (s) =>
+  is_null(tail(tail(tail(s)))) ?
+    addend(tail(s))
+  : make_sum(addend(tail(s)), augend(tail(s)));
+
+const multiplicand = (s) =>
+  is_null(tail(tail(tail(s)))) ?
+    multiplier(tail(s))
+  : make_product(multiplier(tail(s)), multiplicand(tail(s)));
+
+display(deriv(list('*', 'x', 'y', list('+', 'x', 3)), 'x'));
