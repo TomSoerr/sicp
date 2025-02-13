7522edf3c3166d90a1188c2f845a86a74fca6327
TomSoerr <tomix2004@gmail.com>
2025-02-13 22:28:16 +0100

    Add solution 2.93

---
+++ 2-chapter/Extended-Exercise.js
@@ -0,0 +1,128 @@
+import {
+  list,
+  is_undefined,
+  apply_in_underlying_javascript,
+  error,
+  map,
+  display,
+} from 'sicp';
+
+import {
+  install_real_package,
+  install_is_equal_package,
+  install_polynomial_package,
+  install_rational_package,
+  install_term_package,
+} from './Extended-Exercise-Packages.js';
+
+import { get, put, type_tag, contents } from './table-helper.js';
+
+//############################################################################//
+//                                                                            //
+//                             Generic operations                             //
+//                                                                            //
+//############################################################################//
+
+const add = (x, y) => apply_generic('add', list(x, y));
+const mul = (x, y) => apply_generic('mul', list(x, y));
+const sub = (x, y) => apply_generic('sub', list(x, y));
+const div = (x, y) => apply_generic('div', list(x, y));
+const negate = (x) => apply_generic('neg', list(x));
+
+const make_real = (n) => get('make', 'real')(n);
+const make_rational = (n, d) => get('make', 'rational')(n, d);
+const is_equal = (x, y) => apply_generic('is_equal', list(x, y));
+
+const make_polynomial = (variable, terms) =>
+  get('make', 'polynomial')(variable, terms);
+const make_term = (order, coeff) => get('make', 'term')(order, coeff);
+const make_term_list = (l) => get('make', 'term_list')(l);
+
+//############################################################################//
+//                                                                            //
+//                                installation                                //
+//                                                                            //
+//############################################################################//
+
+install_rational_package(put, get, apply_generic);
+install_is_equal_package(put, get, apply_generic);
+install_polynomial_package(put, get, apply_generic);
+install_term_package(put, get, apply_generic);
+
+// order is important
+const tower = list(install_real_package(put, get));
+
+//############################################################################//
+//                                                                            //
+//                                  solution                                  //
+//                                                                            //
+//############################################################################//
+
+const p1 = make_polynomial(
+  'x',
+  make_term_list(list(make_term(5, make_real(1)), make_term(0, make_real(-1)))),
+);
+const p2 = make_polynomial(
+  'x',
+  make_term_list(list(make_term(2, make_real(1)), make_term(0, make_real(-1)))),
+);
+const z1 = make_polynomial('z', make_term_list(list(make_term(3, p1))));
+const z2 = make_polynomial('z', make_term_list(list(make_term(3, p2))));
+
+// try {
+//   add(p1, p2);
+// } catch (error) {
+//   display(error, 'addition tests failed');
+// }
+// try {
+//   add(z1, z2);
+// } catch (error) {
+//   display(error, 'addition where coeffs are polys tests failed');
+// }
+// try {
+//   mul(p1, p2);
+//   mul(z1, z2);
+// } catch (error) {
+//   display(error, 'multiplication tests failed');
+// }
+// try {
+//   sub(p1, p2);
+//   sub(z1, z2);
+// } catch (error) {
+//   display(error, 'subtraction tests failed');
+// }
+// try {
+//   div(p1, p2);
+// } catch (error) {
+//   display(error, 'division tests failed');
+// }
+
+function apply_generic(op, args) {
+  const apply = (fun, args) => apply_in_underlying_javascript(fun, args);
+  const type_tags = map(type_tag, args);
+  const fun = get(op, type_tags);
+
+  if (!is_undefined(fun)) {
+    try {
+      return apply(fun, map(contents, args));
+    } catch (err) {
+      debugger;
+      error(err, 'apply_generic');
+    }
+  } else {
+    debugger;
+    error(list(op, type_tags), 'no method for these types');
+  }
+}
+
+const p3 = make_polynomial(
+  'x',
+  make_term_list(list(make_term(5, make_real(1)), make_term(0, make_real(-1)))),
+);
+const p4 = make_polynomial(
+  'x',
+  make_term_list(list(make_term(2, make_real(1)), make_term(0, make_real(-1)))),
+);
+const rf = make_rational(p3, p4);
+
+// display(rf);
